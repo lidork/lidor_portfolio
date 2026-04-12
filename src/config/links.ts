2c1b36e3e152ace15a7aafeb7b5ab839d6d279ca
Lidor Kalfon <facings.hogan.60@icloud.com>
2026-04-12 13:37:59 +0300

    fix: location

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,7 +1,7 @@
 export const EXTERNAL_LINKS = {
   GITHUB: 'https://github.com/lidork',
   LINKEDIN: 'https://www.linkedin.com/in/lidor-kalfon/',
-  MAP_LOCATION: 'https://maps.google.com/?q=TODO+Location',
+  MAP_LOCATION: 'https://www.google.com/maps/place/%D7%AA%D7%9C+%D7%90%D7%91%D7%99%D7%91-%D7%99%D7%A4%D7%95%E2%80%AD/@32.0879315,34.797246,12z/data=!3m1!4b1!4m6!3m5!1s0x151d4ca6193b7c1f:0xc1fb72a2c0963f90!8m2!3d32.0852999!4d34.7817676!16zL20vMDdxenY?entry=ttu&g_ep=EgoyMDI2MDQwOC4wIKXMDSoASAFQAw%3D%3D',
   EMAIL: 'kalfonlidor@gmail.com',
   CV: `${import.meta.env.BASE_URL}assets/files/Lidor_CV_2026.pdf`,
   SUIKA: 'https://github.com/lidork/Suika_Dungeon',
